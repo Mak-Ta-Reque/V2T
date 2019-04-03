08561bd83ad4a718dc38bc14734f77a7684c6d16
Mak-Ta-Reque <maktareq@gmail.come>
2019-04-03 18:37:04 +0200

    This is initial upload

---
+++ image_2_text.py
@@ -0,0 +1,16 @@
+'''Author: Md Abdul kadir
+period: During MS in Saarland University'''
+
+import tensorflow as tf
+import matplotlib.pyplot as plt
+print ('Tensorflow version: ', tf.__version__)
+from sklearn.model_selection import train_test_split
+from sklearn.utils import shuffle
+import re
+import numpy as np
+import os
+import time
+import json
+from glob import glob
+from PIL import Image
+import pickle
